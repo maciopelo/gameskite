8544705a1307b59c469cb08a9d57c5e0ea96471f
maciopelo <maciej.k00h@gmail.com>
2021-04-30 15:19:30 +0200

    handled basic login and register

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,69 +1,225 @@
 import React, { useState } from "react";
 import Axios from "axios";
 import "../styles/loginPage.scss";
+//import { useHistory } from "react-router";
+import { Formik, Form, useField } from "formik"
+import * as Yup from "yup"
 
 const LoginPage = () => {
-  const [email, setEmail] = useState("");
-  const [nickname, setNickname] = useState("");
-  const [password, setPassword] = useState("");
+  // const [emailReg, setEmailReg] = useState("");
+  // const [nicknameReg, setNicknameReg] = useState("");
+  // const [passwordReg, setPasswordReg] = useState("");
+  // const [passwordLog, setPasswordLog] = useState("");
+  // const [emailLog, setEmailLog] = useState("");
+  const [loginStatus, setLoginStatus] = useState("");
+  const [registerStatus, setRegisterStatus] = useState("");
+  
 
-  const handleRegister = () => {
+  const handleRegister = (values) => {
     Axios.post("http://localhost:8080/register", {
-      email: email,
-      nick: nickname,
-      password: password,
+      email: values.emailReg,
+      nick: values.nick,
+      password: values.passwordReg,
     }).then((res) => {
-      console.log(res);
+      if (res.data.message){
+        setRegisterStatus(res.data.message)
+      }
     });
   };
 
+
+  const handleLogin = (values) => {
+    Axios.post("http://localhost:8080/login", {
+      email: values.emailLog,
+      password: values.passwordLog,
+    }).then((res) => {
+      if (res.data.message){
+        setLoginStatus(res.data.message)
+      }
+      else{
+        setLoginStatus(res.data[0].nick)
+      }
+      
+    });
+  };
+
+  const CustomTextInput = ({label, ...props}) =>{
+    const [field, meta] = useField(props);
+
+    return (
+      <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ): null}
+      </>
+    )
+  }
+
+
   return (
-    <div className="login-page">
-      <div className="login-panel">
-        <h1>Login</h1>
-        <label>
-          e-mail
-          <input placeholder="e-mail" type="email" />
-        </label>
+    <div className = "login-page">
+    <Formik
+    initialValues={{
+      nick: "",
+      emailReg: "",
+      passwordReg: "",
+      confirmPasswordReg: "",
+      file: null,
+    }}
 
-        <label>
-          password
-          <input placeholder="password" type="password" />
-        </label>
-        <button>Login</button>
-      </div>
+    validationSchema = {Yup.object({
+      nick: Yup.string()
+      .min(2, "Must be at least 2 characters")
+      .max(30, "Must be at 30 characters or less")
+      .required("Required"),
+      emailReg: Yup.string()
+      .email("Invalid email address")
+      .required("Required"),
+      passwordReg: Yup.string()
+      .matches(
 
-      <div className="register-panel">
-        <h1>Register</h1>
-        <label>
-          e-mail
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+            "Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+          )
+      .required("Required"),
+      confirmPasswordReg: Yup.string()
+      .oneOf([Yup.ref("passwordReg"), null], "Password must match")
+      .required("Required")
 
-        <label>
-          nickname
-          <input
-            type="text"
-            value={nickname}
-            onChange={(e) => setNickname(e.target.value)}
-          />
-        </label>
+    })}
 
-        <label>
-          password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <button onClick={handleRegister}>Register</button>
-      </div>
+    onSubmit={(values, {setSubmitting, resetForm}) =>{
+      setTimeout(() => {
+        handleRegister(values)
+        resetForm();
+        setSubmitting(false);
+      }, 2000)
+    }}
+    >
+      
+      
+      {props => (
+        
+          
+
+        <Form className="register-panel">
+
+          <h1>Register</h1>
+          <CustomTextInput label="Nick" name = "nick" type="text" placeholder="nick" />
+          <CustomTextInput label="Password" name = "passwordReg" type="password" placeholder="smth" />
+          <CustomTextInput label="Confirm Password" name = "confirmPasswordReg" type="password" placeholder="smth" />
+          <CustomTextInput label="Email" name = "emailReg" type="email" placeholder="smth@smth.com" />
+          <button type = "submit">{props.isSubmitting ? "Loading..." : "Submit"}</button>
+
+          <h1>{registerStatus}</h1>
+
+        </Form>
+
+       
+      )}
+      
+    </Formik>
+        <Formik
+    initialValues={{
+      emailLog: "",
+      passwordLog: "",
+    }}
+
+    validationSchema = {Yup.object({
+      emailLog: Yup.string()
+      .email("Invalid email address")
+      .required("Required"),
+      passwordLog: Yup.string()
+      .required("Required"),
+    })}
+
+    onSubmit={(values, {setSubmitting, resetForm}) =>{
+      setTimeout(() => {
+        handleLogin(values)
+        resetForm();
+        setSubmitting(false);
+      }, 2000)
+    }}
+    >
+      
+      
+      {props => (
+        
+          
+        <Form className="login-panel">
+          <h1>Log in</h1>
+
+          <CustomTextInput label="Email" name = "emailLog" type="email" placeholder="smth@smth.com" />
+          <CustomTextInput label="Password" name = "passwordLog" type="password" placeholder="smth" />
+          <button type = "submit">{props.isSubmitting ? "Loading..." : "Submit"}</button>
+
+          <h1>{loginStatus}</h1>
+
+        </Form>
+
+     
+      )}
+      
+    </Formik>
     </div>
+  
+
+
+    // <div className="login-page">
+    //   <div className="login-panel">
+    //     <h1>Login</h1>
+    //     <label>
+    //       e-mail
+    //       <input placeholder="e-mail" type="email" 
+    //       value = {emailLog}
+    //       onChange={(e) => setEmailLog(e.target.value)}
+    //       />
+    //     </label>
+
+    //     <label>
+    //       password
+    //       <input placeholder="password" type="password" 
+    //       value = {passwordLog}
+    //       onChange={(e) => setPasswordLog(e.target.value)}
+    //       />
+    //     </label>
+    //     <button onClick={handleLogin}>Login</button>
+    //   </div>
+    //   
+
+    //   <div className="register-panel">
+    //     <h1>Register</h1>
+    //     <label>
+    //       e-mail
+    //       <input
+    //         type="email"
+    //         value={emailReg}
+    //         onChange={(e) => setEmailReg(e.target.value)}
+    //       />
+    //     </label>
+
+    //     <label>
+    //       nickname
+    //       <input
+    //         type="text"
+    //         value={nicknameReg}
+    //         onChange={(e) => setNicknameReg(e.target.value)}
+    //       />
+    //     </label>
+
+    //     <label>
+    //       password
+    //       <input
+    //         type="password"
+    //         value={passwordReg}
+    //         onChange={(e) => setPasswordReg(e.target.value)}
+    //       />
+    //     </label>
+    //     <button onClick={handleRegister}>Register</button>
+    //   </div>
+    // </div>
   );
 };
 
--- server/server.js
+++ server/server.js
@@ -20,6 +20,8 @@ const db = mysql.createConnection({
   database: process.env.LOCAL_DB_NAME,
 });
 
+
+
 /* remote db */
 // const db = mysql.createConnection({
 //   host: process.env.REMOTE_DB_HOST,
@@ -37,16 +39,95 @@ db.connect((err) => {
 });
 
 app.post("/register", (req, res) => {
+
+  console.log(req.body)
+  
+  
   const { email, nick, password } = req.body;
 
-  const query = "INSERT INTO users (nick, email, password) VALUES (?,?,?)";
+  const queryNick = "SELECT * FROM users WHERE nick = ?";
 
-  db.query(query, [nick, email, password], (err, result) => {
-    console.log(err);
-    console.log(result);
+  const queryEmail = "SELECT * FROM users WHERE email = ?";
+
+  const queryReg = "INSERT INTO users (nick, email, password) VALUES (?,?,?)";
+
+
+  db.query(queryNick, [nick], (errNick, resultNick) => {
+    if (errNick){
+      res.send({err: errNick});
+    }
+    if(resultNick.length > 0){
+      console.log("Username taken")
+      res.send({message: "Username taken"});
+    }
+    else {
+
+      db.query(queryEmail, [email], (errEmail, resultEmail) => {
+        if (errEmail){
+          res.send({err: errEmail});
+        }
+        
+
+
+        if(resultEmail.length > 0){
+          console.log("Email taken")
+          res.send({message: "Email taken"});
+
+        }
+        else {
+
+          db.query(queryReg, [nick, email, password], (errReg, resultReg) => {
+          console.log("less goo")
+          console.log(errReg);
+          console.log(resultReg);
+          res.send({message: "Succesfull Registration!"});
+
+          });
+
+
+        }
+
+      });
+
+    }
+
+  });
+
+
+
+
+
+  
+
+
+
+});
+
+app.post("/login", (req, res) => {
+  const {email, password} = req.body;
+
+  const query = "SELECT * FROM users WHERE email = ? AND password = ?";
+
+  db.query(query, [email, password], (err, result) => {
+    if(err) {
+      res.send({err: err});
+    }
+
+    if(result.length > 0){
+      res.send(result);
+    }
+    else {
+      res.send({message: "Wrong username or password"})
+    }
+      
+  
   });
 });
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
+
